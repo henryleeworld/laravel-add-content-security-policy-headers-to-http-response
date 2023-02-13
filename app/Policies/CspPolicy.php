2eabf81ffd967ffabc4f59e70fe9ed934cc9a2e4
Henry Lee <henryleeworld@gmail.com>
2023-02-13 08:57:00 +0800

    feat:初始化

---
+++ app/Policies/CspPolicy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Policies;
+ 
+use Spatie\Csp\Directive;
+use Spatie\Csp\Policies\Basic;
+ 
+class CspPolicy extends Basic
+{
+    public function configure()
+    {
+        parent::configure();
+
+        $this->addDirective(Directive::FONT, ['fonts.bunny.net'])
+             ->addDirective(Directive::STYLE, ['fonts.bunny.net']);
+    }
+}
